20167c6917b81830fd823741a714216b1ea768af
Valentin ROHR <valentin.rohr9@etu.univ-lorraine.fr>
2026-06-04 11:57:49 +0200

    config import

--- HTTP_SERVER_SAE/src/ServerWeb/config/ConfigSite.java
+++ HTTP_SERVER_SAE/src/ServerWeb/config/ConfigSite.java
@@ -1,4 +1,11 @@
 package ServerWeb.config;
+import javax.xml.*;
+import org.w3c.dom.*;
 
 public class ConfigSite {
+
+    DocumentBuilderFactory factory = DocumentBilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    Document doc = builder.parse(new File("serverWeb.conf"));
+    NodeList list = doc.getElementsByTagName("person");
 }
